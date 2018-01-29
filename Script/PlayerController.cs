4a2d8fa179a752fab84672aadbc17a11fe2d1e9c
enzo34 <eclavelloux@gmail.com>
2018-01-29 19:26:24 +0100

    Add files via upload

---
+++ Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    // Déclarations de variables
+    public int speed = 7;
+    public int jump = 300;
+    public bool isGrounded = false;
+
+    private bool isJumping = false;
+    private Animator Anim;
+
+    // A l'initialisation du jeu
+	void Start ()
+    {
+        Anim = GetComponent<Animator>();    
+	}
+	
+
+    // Toute les frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * h * speed * Time.deltaTime);
+	}
+
+
+    //Pour lancer à un moment précis et éviter de surcharger le jeu
+    void FixedUpdate ()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jump * Time.deltaTime;
+        }
+    }
+}

9d1a8df2deafb34dffcce4e4b66b9c8ed8a0505e
enzo34 <eclavelloux@gmail.com>
2018-01-29 20:15:31 +0100

    Work in Progress

---
+++ Script/IsGroundedScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IsGroundedScript : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D col)
+    {
+        transform.parent.GetComponent<PlayerController>().isGrounded = true;
+	}
+
+    void OnTriggerExit2D (Collider2D col)
+    {
+        transform.parent.GetComponent<PlayerController>().isGrounded = false;
+    }
+}
--- Script/PlayerController.cs
+++ Script/PlayerController.cs
@@ -22,15 +22,32 @@ public class PlayerController : MonoBehaviour {
     {
         float h = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * h * speed * Time.deltaTime);
+
+        if(h > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            Anim.SetBool("Walk", true);
+        }
+        if(h < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            Anim.SetBool("Walk", true);
+        }
+        if(h == 0)
+        {
+            Anim.SetBool("Walk", false);
+        }
 	}
 
 
     //Pour lancer à un moment précis et éviter de surcharger le jeu
     void FixedUpdate ()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * jump * Time.deltaTime;
+            Anim.SetTrigger("Jump");
+            Anim.SetBool("Walk", false);
         }
     }
 }

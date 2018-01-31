6af0a724c73354db8c72d8c4899b0687db7f4bda
enzo34 <eclavelloux@gmail.com>
2018-01-31 12:39:21 +0100

    Finalistaion de la parallax

---
+++ Script/ScriptParallax/ParallaxScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScript : MonoBehaviour {
+
+    private float offset;
+    public GameObject Player;
+    public int speed;
+	
+	void Update ()
+    {
+        offset = Player.transform.position.x / speed;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+}

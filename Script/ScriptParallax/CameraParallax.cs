3bd2f73398f5c2c12594e0624fe869d4d0745b04
enzo34 <eclavelloux@gmail.com>
2018-01-31 11:53:44 +0100

    Mise en place des Layers

---
+++ Script/ScriptParallax/CameraParallax.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraParallax : MonoBehaviour {
+
+    public GameObject Player;
+	
+	void Update ()
+    {
+        transform.position = new Vector3(Player.transform.localPosition.x, transform.position.y, transform.position.z);	
+	}
+}

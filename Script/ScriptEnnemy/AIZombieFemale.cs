52e9854d32b0d6dc315f14c66df94badc71987a9
enzo34 <eclavelloux@gmail.com>
2018-02-03 09:30:39 +0100

    Script Zombie Female Attack

---
+++ Script/ScriptEnnemy/AIZombieFemale.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIZombieFemale : MonoBehaviour {
+
+    public GameObject target;
+    public float distance;
+    public float RangeAttack = 5;
+    public float speed = 0.5f;
+    private Animator Anim;
+
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance < RangeAttack)
+        {
+            Attack();
+        }
+
+        if (transform.position.x > target.transform.position.x)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        } else
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+	}
+
+    void Attack()
+    {
+        transform.position = Vector2.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}

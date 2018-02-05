f8f18bb29d5a32c1a59f77c33dd8cc2cf5be7306
enzo34 <eclavelloux@gmail.com>
2018-02-05 10:51:21 +0100

    Animation Zombie female

--- Script/ScriptEnnemy/AIZombieFemale.cs
+++ Script/ScriptEnnemy/AIZombieFemale.cs
@@ -7,6 +7,7 @@ public class AIZombieFemale : MonoBehaviour {
     public float distance;
     public float RangeAttack = 5;
     public float speed = 0.5f;
+    public AudioClip SoundDead, SoundHit;
     private Animator Anim;
 
 	void Start () {
@@ -18,7 +19,11 @@ public class AIZombieFemale : MonoBehaviour {
 
         if (distance < RangeAttack)
         {
+            Anim.SetBool("Walk", true);
             Attack();
+        } else
+        {
+            Anim.SetBool("Walk", false);
         }
 
         if (transform.position.x > target.transform.position.x)
@@ -33,5 +38,20 @@ public class AIZombieFemale : MonoBehaviour {
     void Attack()
     {
         transform.position = Vector2.Lerp(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag=="Player")
+        {
+            Anim.SetTrigger("Dead");
+            GetComponent<AudioSource>().PlayOneShot(SoundDead);
+            GetComponent<AIZombieFemale>().enabled = false;
+        }
+    }
+
+    public void DestroyGO()
+    {
+        Destroy(gameObject);
     }
 }

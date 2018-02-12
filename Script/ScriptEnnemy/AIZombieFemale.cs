90258cde12845ce4be346d8bce2d609bb88a7ad1
enzo34 <eclavelloux@gmail.com>
2018-02-12 13:49:28 +0100

    Script Degats Player et Script Zombie Male

--- Script/ScriptEnnemy/AIZombieFemale.cs
+++ Script/ScriptEnnemy/AIZombieFemale.cs
@@ -8,7 +8,10 @@ public class AIZombieFemale : MonoBehaviour {
     public float RangeAttack = 5;
     public float speed = 0.5f;
     public AudioClip SoundDead, SoundHit;
+
     private Animator Anim;
+    private bool pause = false;
+    private bool dead = false;
 
 	void Start () {
         Anim = GetComponent<Animator>();
@@ -17,7 +20,7 @@ public class AIZombieFemale : MonoBehaviour {
 	void Update () {
         distance = Vector2.Distance(transform.position, target.transform.position);
 
-        if (distance < RangeAttack)
+        if (distance < RangeAttack && !pause)
         {
             Anim.SetBool("Walk", true);
             Attack();
@@ -42,10 +45,11 @@ public class AIZombieFemale : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.gameObject.tag=="Player")
+        if(col.gameObject.tag=="Player" && !dead)
         {
             Anim.SetTrigger("Dead");
             GetComponent<AudioSource>().PlayOneShot(SoundDead);
+            dead = true;
             GetComponent<AIZombieFemale>().enabled = false;
         }
     }
@@ -53,5 +57,30 @@ public class AIZombieFemale : MonoBehaviour {
     public void DestroyGO()
     {
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player" && !dead)
+        {
+            StartCoroutine(Hit(col.gameObject));
+        }
+    }
+
+    IEnumerator Hit(GameObject col)
+    {
+        Debug.Log("Player touched");
+        GetComponent<AudioSource>().PlayOneShot(SoundHit);
+        pause = true;
+        if (transform.position.x > target.transform.position.x)
+        {
+            col.GetComponent<Rigidbody2D>().velocity = (-Vector2.right * 200 * Time.deltaTime);
+        }
+        else
+        {
+            col.GetComponent<Rigidbody2D>().velocity = (Vector2.right * 200 * Time.deltaTime);
+        }
+        yield return new WaitForSeconds(1);
+        pause = false;
     }
 }
---
+++ Script/ScriptEnnemy/AIZombieMale.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIZombieMale : MonoBehaviour {
+    public Transform PtsA;
+    public Transform PtsB;
+    public float speed = 0.5f;
+    private bool Retour = false;
+
+    void Start () {
+	
+	}
+
+
+    void Update () {
+	    if (!Retour)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, PtsB.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, PtsA.position, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name =="PtsA")
+        {
+            Retour = false;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (col.gameObject.name == "PtsB")
+        {
+            Retour = true;
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
